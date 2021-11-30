8890d2c20be9ad14a9ebbab0a1df5e9153720ea0
Saeed Jabbar <saeedryanjabbar@gmail.com>
2021-11-30 17:26:48 -0500

    fb added

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,3 +1,6 @@
+import firebase from 'firebase/app';
+
+
 const firebaseConfig = {
   apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
   authDomain: `${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
@@ -6,3 +9,6 @@ const firebaseConfig = {
   messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}`,
   appId: `${process.env.REACT_APP_FIREBASE_APP_ID}`,
 };
+
+firebase.initializeApp(firebaseConfig);
+export default firebase;
