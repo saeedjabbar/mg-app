941b3f0de296f0386011f2ad12b2866c893b8880
Saeed Jabbar <saeedryanjabbar@gmail.com>
2021-12-01 00:21:06 -0500

    fb config working

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { firebase, FieldValue } from './lib/firebase';
-import FirebaseContext from './context/firebase';
+import FirebaseContext from './context/firebaseContext';
 
 ReactDOM.render(
   <FirebaseContext.Provider value={{ firebase, FieldValue }}>
--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,15 +1,29 @@
-import { initializeApp } from "firebase/app";
-import { FieldValue } from "firebase/firestore";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
+import { seedDatabase } from '../seed';
+
 
 const firebaseConfig = {
-  apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
-  authDomain: `${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
-  projectId: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`,
-  storageBucket: `${process.env.REACT_APP_FIREBASE_STORAGE_BUCKET}`,
-  messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}`,
-  appId: `${process.env.REACT_APP_FIREBASE_APP_ID}`,
-};
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+}
 
-const firebase = initializeApp(firebaseConfig);
+console.log(process.env.REACT_APP_FIREBASE_API_KEY);
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore();
+
+console.log('firebase', firebase);
+console.log(firebaseConfig);
+
+
+const FieldValue = firebase.firestore.FieldValue;
+seedDatabase(firebase);
 
 export { firebase, FieldValue };
